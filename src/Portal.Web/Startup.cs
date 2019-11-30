e15808feb4e2e71d8628002e4f884b3926ea4787
Hakim Ghods <ghods.hakim@hotmail.com>
2019-11-30 10:40:33 +0330

    Logging

--- src/Portal.Web/Startup.cs
+++ src/Portal.Web/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Portal.Application.Common;
 using Portal.Application.FoodApplication;
 using Portal.Application.FoodApplication.Commands.Create;
 using Portal.Application.Foods;
@@ -47,6 +48,8 @@ namespace Portal.Web
 
             services.AddAutoMapper(typeof(FoodMapper).GetTypeInfo().Assembly);
             services.AddMediatR(typeof(FoodCreateCommand).GetTypeInfo().Assembly);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
             services.AddTransient<IFoodService, FoodService>();
 
