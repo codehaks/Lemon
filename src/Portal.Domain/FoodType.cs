231979fb9dec7154cc2f9ebb77913bcc347120bd
Hakim Ghods <ghods.hakim@hotmail.com>
2019-12-11 04:00:13 +0330

    Fix

--- src/Portal.Domain/FoodType.cs
+++ src/Portal.Domain/FoodType.cs
@@ -1,9 +1,20 @@
-﻿namespace Portal.Common.Enums
+﻿using Portal.Domain.Common;
+
+namespace Portal.Common.Enums
 {
-    public enum FoodType
+    public class FoodType : Enumeration
     {
-        Meal = 0,
-        Drink = 1,
-        Desert = 2
+        //Meal = 0,
+        //Drink = 1,
+        //Desert = 2
+
+        public static FoodType Meal = new FoodType(0, "Meal");
+        public static FoodType Drink = new FoodType(1, "Drink");
+        public static FoodType Desert = new FoodType(2, "Desert");
+
+        public FoodType(int id, string name)
+            : base(id, name)
+        {
+        }
     }
 }
--- src/Portal.Infrastructure/Persistance/Configs/FoodConfig.cs
+++ src/Portal.Infrastructure/Persistance/Configs/FoodConfig.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Portal.Common.Enums;
 using Portal.Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Portal.Persistance.Configs
@@ -14,6 +16,10 @@ namespace Portal.Persistance.Configs
             builder.HasIndex(f => f.Name).IsUnique();
             builder.Property(f => f.Name).HasMaxLength(25).IsRequired();
             builder.Property(f => f.Description).HasMaxLength(1000).IsRequired();
+            builder.Property(f=>f.FoodType)
+                .HasColumnName("FoodType")
+                .HasConversion(v => v.Id,
+                v => FoodType.GetAll<FoodType>().Single(s => s.Id==v));
         }
     }
 }
