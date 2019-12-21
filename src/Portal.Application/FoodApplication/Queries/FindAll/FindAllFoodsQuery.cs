634d75bdc1d1837fb7dffedd482456722a8b9f2b
Hakim Ghods <ghods.hakim@hotmail.com>
2019-12-21 17:05:27 +0330

    ups

--- src/Portal.Application/FoodApplication/Queries/FindAll/FindAllFoodsQuery.cs
+++ src/Portal.Application/FoodApplication/Queries/FindAll/FindAllFoodsQuery.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Portal.Application.Foods.Models;
+using Portal.Common.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Portal.Application.FoodApplication.Queries.FindAll
 {
     public class FindAllFoodsQuery:IRequest<IList<FoodInfo>>
     {
+        public FoodType FoodType { get; set; }
     }
 }
