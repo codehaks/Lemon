1ac4855d83253af13c94b40bc29b49813698a92a
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-21 01:42:26 +0330

    fix

---
+++ src/Portal.Web/Areas/Admin/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portal.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Portal.Web.Areas.Admin.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+     
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public IndexModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+           
+        }
+
+    
+
+        public IList<ApplicationUser> List { get; set; }
+        public string Term { get; set; }
+
+        public void OnGet(string term = "")
+        {
+            
+            if (string.IsNullOrEmpty(term))
+            {
+                List = _userManager
+                .Users
+             
+                .ToList();
+            }
+            else
+            {
+                List = _userManager
+                .Users
+                .Where(u => u.UserName.StartsWith(term))
+                .ToList();
+            }
+
+            Term = term;
+         
+        }
+    }
+}
