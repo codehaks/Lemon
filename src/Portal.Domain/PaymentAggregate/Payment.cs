9df041510044d2d22582fcf9a92b4f276fab5b92
Hakim Ghods <ghods.hakim@hotmail.com>
2020-03-05 02:14:04 +0330

    fix

---
+++ src/Portal.Domain/PaymentAggregate/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.PaymentAggregate
+{
+    class Payment
+    {
+        public string UserId { get; set; }
+        public int Amount { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ src/Portal.Domain/PaymentAggregate/PaymentEventSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.PaymentAggregate
+{
+    class PaymentEventSource
+    {
+        public Guid Id { get; private set; }
+
+        public string Data { get; private set; }
+
+        public string User { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public PaymentEventType EventType { get; set; }
+    }
+
+    public enum PaymentEventType
+    {
+        AddCredit=0,
+        RemoveCredit=1
+    }
+}
