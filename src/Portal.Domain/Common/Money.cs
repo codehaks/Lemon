4d4dea7a729f7be46f218934122c20153af95bb9
Hakim Ghods <ghods.hakim@hotmail.com>
2020-03-05 12:14:49 +0330

    fix

--- src/Portal.Domain/Common/Money.cs
+++ src/Portal.Domain/Common/Money.cs
@@ -7,6 +7,10 @@ namespace Portal.Domain.Values
 {
     public class Money
     {
+        public Money()
+        {
+
+        }
         public Money(int value)
         {
             if (value >= 0)
@@ -18,7 +22,7 @@ namespace Portal.Domain.Values
                 throw new ArgumentOutOfRangeException("پول نمی تواند کمتر از 0 باشد!");
             }
         }
-        public int Value { get;}
+        public int Value { get; private set; }
 
     }
 }
